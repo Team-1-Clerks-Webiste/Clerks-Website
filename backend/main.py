1162aaf801f1adf6ee9e1b4624f1463edf534f37
Jayden Lloyd <jaydenlloyd017@.com>
2026-03-27 12:09:55 +0000

    tested api endpoint for main.py

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,6 @@
 from fastapi import FastAPI
+from models.login_endpoint import router as auth_router
+
 
 from schemas.user import init_users_table
 
@@ -6,3 +8,7 @@ app = FastAPI()
 
 
 init_users_table()
+
+@app.get("/")
+def home():
+    return {"message": "Welcome to Clerks API"}
