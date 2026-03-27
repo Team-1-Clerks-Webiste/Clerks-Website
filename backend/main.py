8f9d229cc717a855da7b9f8dcbf5b2ec2c6d28fd
Jayden Lloyd <jaydenlloyd017@.com>
2026-03-27 14:01:54 +0000

    linked login to main

--- backend/main.py
+++ backend/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from schemas.user import init_users_table
 from schemas.shoe import init_shoes_table
 from models.shoe_endpoint import router as shoes_router
+from models.login_endpoint import router as auth_router
 
 app = FastAPI()
 
@@ -11,6 +12,7 @@ init_shoes_table()
 
 # link routes
 app.include_router(shoes_router)
+app.include_router(auth_router)
 
 @app.get("/")
 def home():
