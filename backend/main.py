f1647968c403bc1f88dbce52db2d7a16565be9f2
Jayden Lloyd <jaydenlloyd017@.com>
2026-03-27 14:15:48 +0000

    api for shoes now working

--- backend/main.py
+++ backend/main.py
@@ -3,12 +3,14 @@ from schemas.user import init_users_table
 from schemas.shoe import init_shoes_table
 from models.shoe_endpoint import router as shoes_router
 from models.login_endpoint import router as auth_router
+from schemas.shoe import init_shoes_table, seed_shoes
 
 app = FastAPI()
 
 # create tables on startup
 init_users_table()
 init_shoes_table()
+seed_shoes()
 
 # link routes
 app.include_router(shoes_router)
