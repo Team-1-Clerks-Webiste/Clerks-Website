af1e2853b270d3200169b1d548d56357b2214eab
Jayden Lloyd <jaydenlloyd017@.com>
2026-03-27 12:12:16 +0000

    added notes

--- backend/main.py
+++ backend/main.py
@@ -7,8 +7,16 @@ from schemas.user import init_users_table
 app = FastAPI()
 
 
+
+
+#create tables on startup
 init_users_table()
+
+
+#link the routes
+app.include_router(auth_router)
 
 @app.get("/")
 def home():
     return {"message": "Welcome to Clerks API"}
+
