f0b9a6e44b009fb49ae54f6e8c6606f9fef2f776
revilo-008 <purple2000turtle@gmail.com>
2026-03-27 12:33:02 +0000

    mostly done

---
+++ backend/models/login_endpoint.py
@@ -0,0 +1,23 @@
+from fastapi import FastAPI as app
+
+users = [
+    {"id": 1,"username":"user 1", "password":"pass", "email":"example@example.com","created_at":"1/1/1"},
+    {"id": 2,"username":"user 2", "password":"password_1234", "email":"example@other_example.com","created_at":"1/1/1"},
+    {"id": 3,"username":"John Smith", "password":"realy_strong_password", "email":"you@example.com","created_at":"1/1/1"},
+         ]
+
+def get_db():
+    conn = sqlite3.connect("clerksdb.db")
+    conn.row_factory = sqlite3.row
+    return conn
+
+@app.get("/users")
+def get_user():
+    conn = get_db()
+    users = conn.execute("Select username, password, email From users")
+    
+
+
+
+
+
